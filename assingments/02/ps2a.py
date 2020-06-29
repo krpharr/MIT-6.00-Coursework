1bc043993ffc9abe86280ac6f8a93906d998491f
rpharr <rpharr@richmond.edu>
2020-06-29 15:14:53 -0400

    set size defaults so can be easily changed by arguments

--- assingments/02/ps2a.py
+++ assingments/02/ps2a.py
@@ -1,39 +1,39 @@
 
-def solve(numNuggets):
+def solve(numNuggets, SMALL=6, MEDIUM=9, LARGE=20):
 	nSm = 0
 	nMed = 0
 	nLrg = 0
 	#solve for large
-	if numNuggets >= 20:
-		if numNuggets%20 == 0:
-			return (nSm, nMed, numNuggets/20)
-		for i in range(0, numNuggets//20):
+	if numNuggets >= LARGE:
+		if numNuggets%LARGE == 0:
+			return (nSm, nMed, numNuggets/LARGE)
+		for i in range(0, numNuggets//LARGE):
 			nLrg = i + 1
-			s = solve(numNuggets-(nLrg*20))
+			s = solve(numNuggets-(nLrg*LARGE))
 			if s != (None, None, None):
 				return (s[0], s[1], nLrg+s[2])
 		nLrg = 0
 	#solve for med
-	if numNuggets >= 9:
-		if numNuggets%9 == 0:
-			return (nSm, numNuggets/9, nLrg)
-		for i in range(0, numNuggets//9):
+	if numNuggets >= MEDIUM:
+		if numNuggets%MEDIUM == 0:
+			return (nSm, numNuggets/MEDIUM, nLrg)
+		for i in range(0, numNuggets//MEDIUM):
 			nMed = i + 1
-			s = solve(numNuggets-(nMed*9))
+			s = solve(numNuggets-(nMed*MEDIUM))
 			if s != (None, None, None):
 				return (s[0], nMed+s[1], s[2])
 		nMd = 0
 	#solve for small
-	if numNuggets >= 6:
-		if numNuggets%6 == 0:
-			return (numNuggets/6, nSm, nLrg)
-		for i in range(0, numNuggets//6):
+	if numNuggets >= SMALL:
+		if numNuggets%SMALL == 0:
+			return (numNuggets/SMALL, nSm, nLrg)
+		for i in range(0, numNuggets//SMALL):
 			nSm = i + 1
-			s = solve(numNuggets-(nSm*6))
+			s = solve(numNuggets-(nSm*SMALL))
 			if s != (None, None, None):
 				return (nSm+s[0], s[1], s[2])
 		nSm = 0
 	return (None, None, None)
 		
-for i in range(6, 201):
+for i in range(6, 200):
 	print(i, solve(i))

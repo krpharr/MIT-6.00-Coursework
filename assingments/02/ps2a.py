ce472438148b7721bdd758995a828ee1d4f889ad
rpharr <rpharr@richmond.edu>
2020-06-29 15:04:17 -0400

    moved, updated for python3

--- assingments/02/ps2a.py
+++ assingments/02/ps2a.py
@@ -7,7 +7,7 @@ def solve(numNuggets):
 	if numNuggets >= 20:
 		if numNuggets%20 == 0:
 			return (nSm, nMed, numNuggets/20)
-		for i in range(0, numNuggets/20):
+		for i in range(0, numNuggets//20):
 			nLrg = i + 1
 			s = solve(numNuggets-(nLrg*20))
 			if s != (None, None, None):
@@ -17,7 +17,7 @@ def solve(numNuggets):
 	if numNuggets >= 9:
 		if numNuggets%9 == 0:
 			return (nSm, numNuggets/9, nLrg)
-		for i in range(0, numNuggets/9):
+		for i in range(0, numNuggets//9):
 			nMed = i + 1
 			s = solve(numNuggets-(nMed*9))
 			if s != (None, None, None):
@@ -27,16 +27,13 @@ def solve(numNuggets):
 	if numNuggets >= 6:
 		if numNuggets%6 == 0:
 			return (numNuggets/6, nSm, nLrg)
-		for i in range(0, numNuggets/6):
+		for i in range(0, numNuggets//6):
 			nSm = i + 1
 			s = solve(numNuggets-(nSm*6))
 			if s != (None, None, None):
 				return (nSm+s[0], s[1], s[2])
 		nSm = 0
 	return (None, None, None)
-			
-
-
-
+		
 for i in range(6, 201):
-	print i, solve(i)
+	print(i, solve(i))

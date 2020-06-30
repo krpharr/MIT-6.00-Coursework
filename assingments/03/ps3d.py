1875054f2f521a44d6a8a8f10b1b9ec60c0acd04
rpharr <rpharr@richmond.edu>
2020-06-29 22:04:22 -0400

    tests for ps3b

--- assingments/03/ps3d.py
+++ assingments/03/ps3d.py
@@ -1,17 +1,7 @@
 from string import *
 
-# this is a code file that you can use as a template for submitting your
-# solutions
-
-
-# these are some example strings for use in testing your code
-
-#  target strings
-
 target1 = 'atgacatgcacaagtatgcat'
 target2 = 'atgaatgcatggatgtaaatgcag'
-
-# key strings
 
 key10 = 'a'
 key11 = 'atg'
@@ -22,7 +12,7 @@ def subStringMatchExact(target,key):
 	startpoint = ()
 	i = 0
 	while i != -1:
-		i = find(target, key, i)
+		i = target.find(key, i)
 		if i != -1:	
 			startpoint += (i,)
 			i += 1
@@ -73,8 +63,8 @@ def subStringMatchExactlyOneSub(target,key):
 			solution += (oneSub[i],)
 	return solution
 			
-print target1, key12
-print subStringMatchExactlyOneSub(target1,key12)	
+print(target1, key12)
+print(subStringMatchExactlyOneSub(target1,key12))	
 
     
 
--- assingments/03/test_ps3.py
+++ assingments/03/test_ps3.py
@@ -1,7 +1,16 @@
 import unittest
 from ps3a import countSubStringMatch, countSubStringMatchRecursive
+from ps3b import subStringMatchExact
 
-class TestPS3(unittest.TestCase):
+target1 = 'atgacatgcacaagtatgcat'
+target2 = 'atgaatgcatggatgtaaatgcag'
+
+key10 = 'a'
+key11 = 'atg'
+key12 = 'atgc'
+key13 = 'atgca'
+
+class TestPS3a(unittest.TestCase):
 
   def test_countSubStringMatch(self):
     result = countSubStringMatch("qwertyqwertyqwertyqwertyqwerty", "ert")
@@ -14,6 +23,21 @@ class TestPS3(unittest.TestCase):
     self.assertEqual(result, 5)
     result = countSubStringMatchRecursive("qwertyqwertyqwertyqwertyqwerty", "hjkl")
     self.assertEqual(result, 0)
+
+class TestPS3b(unittest.TestCase):
+
+  def test_subStringMatchExact(self):
+    print(target2, key12)
+    result = subStringMatchExact(target2,key12)
+    self.assertEqual(result, (4,18))
+
+    print(target2, key11)
+    result = subStringMatchExact(target2,key11)
+    self.assertEqual(result, (0,4,8,12,18))
+
+    print(target2, "key11")
+    result = subStringMatchExact(target2,"key11")
+    self.assertEqual(result, ())
 
 
 if __name__ == '__main__':
